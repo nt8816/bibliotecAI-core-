9f3813949c9cb5c0f5f182e9df8d8289a8ca615c
Francisco Rai <franciscorai1358@gmail.com>
2026-03-21 23:33:41 -0300

    tirei o google tradutor

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AppErrorBoundary } from "@/components/AppErrorBoundary";
 import "./index.css";
+
+document.documentElement.lang = "pt-BR";
+document.documentElement.setAttribute("translate", "no");
+document.documentElement.classList.add("notranslate");
+document.body?.setAttribute("translate", "no");
+document.body?.classList.add("notranslate");
+document.getElementById("root")?.setAttribute("translate", "no");
+document.getElementById("root")?.classList.add("notranslate");
+
 createRoot(document.getElementById("root")).render(_jsx(AppErrorBoundary, { children: _jsx(App, {}) }));
