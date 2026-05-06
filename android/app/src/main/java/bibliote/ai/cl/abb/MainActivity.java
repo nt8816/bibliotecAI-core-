3a9de0dfe20603fb4787fb5806fff1f7fbde2531
Francisco Rai <franciscorai1358@gmail.com>
2026-05-05 23:48:19 -0300

    Refactor code structure for improved readability and maintainability

---
+++ android/app/src/main/java/bibliote/ai/cl/abb/MainActivity.java
@@ -0,0 +1,5 @@
+package bibliote.ai.cl.abb;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- android/app/src/main/java/com/bibliotecai/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.bibliotecai.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
